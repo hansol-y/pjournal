370f29f12479f308eaed21767eee520dd3c89778
hansolyang <hansol94@student.ubc.ca>
2024-06-03 23:19:23 +0900

    init

---
+++ backend/src/main/java/com/example/pjournal/PJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PJournalApplication.class, args);
+	}
+
+}
