476573835ba93a3f74fc252be68e4eb935ca4344
hansolyang <hansol94@student.ubc.ca>
2024-06-10 20:23:47 +0900

    added initial directories for backend

--- backend/src/main/java/org/example/pjournal/PJournalApplication.java
+++ backend/src/main/java/org/example/pjournal/PJournalApplication.java
@@ -1,13 +1,11 @@
-package com.example.pjournal;
+package org.example.pjournal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PJournalApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PJournalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PJournalApplication.class, args);
+    }
 }
